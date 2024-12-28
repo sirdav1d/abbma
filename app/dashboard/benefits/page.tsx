ea5f125ea10aaf581d9168ed13583fe434d7b6fd
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2024-12-28 04:17:56 -0300

    fix: :lipstick: `Update BenefitsPage component with px-0 on 2xl screen size`

--- app/dashboard/benefits/page.tsx
+++ app/dashboard/benefits/page.tsx
@@ -32,7 +32,7 @@ export default async function BenefitsPage() {
 	});
 
 	return (
-		<div className='mx-auto max-w-7xl w-full mt-5 px-4 md:px=0'>
+		<div className='mx-auto max-w-7xl w-full mt-5 px-4 2xl:px-0'>
 			<div className='flex flex-col gap-10'>
 				<h2 className='font-semibold text-lg md:text-2xl text-pretty'>
 					Meus Benefícios Ativos
