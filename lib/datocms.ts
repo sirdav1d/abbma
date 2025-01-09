d60ea21759d8535f4bc04c6fddbc08431a08bc95
sirdav1d <ddavid.diniz@gmail.com>
2025-01-09 13:24:24 -0300

    feat: :sparkles: integraçaõ datocms com erros

---
+++ lib/datocms.ts
@@ -0,0 +1,44 @@
+import { gql, GraphQLClient } from 'graphql-request';
+
+interface RequestProps {
+	query: string;
+	variables?: unknown;
+	revalidate: number;
+}
+
+export function request({ query, variables, revalidate }: RequestProps) {
+	const endpoint = process.env.NEXT_DATOCMS_ENDPOINT || '';
+	const token = process.env.NEXT_DATOCMS_TOKEN;
+	const graphQLClient = new GraphQLClient(endpoint, {
+		method: 'POST',
+		next: { revalidate: revalidate },
+		headers: {
+			authorization: `Bearer ${token}`,
+		},
+	});
+
+	return graphQLClient.request(query, variables);
+}
+
+export const query = `query MyQuery {
+  abbmaDatabase {
+    videoHero {
+      url
+    }
+    videoHeroThumb {
+      url
+    }
+  }
+}`
+
+
+export interface AbbmaDatabaseProps {
+  abbmaDatabase: {
+    videoHero: {
+		  url: string;
+	  };
+	  videoHeroThumb: {
+		  url: string;
+	  };
+  }	
+}
