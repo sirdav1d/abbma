2ba8d66998213d27bc956e837ae71f5e1a2a6433
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2025-01-14 03:04:15 -0300

    refactor: :lipstick: `Refactor table component: add rounded corners to container div`

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -8,7 +8,7 @@ const Table = React.forwardRef<
 	HTMLTableElement,
 	React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-	<div className='relative w-full overflow-auto'>
+	<div className='relative w-full overflow-auto rounded-xl'>
 		<table
 			ref={ref}
 			className={cn('w-full caption-bottom text-sm', className)}
