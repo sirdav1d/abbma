48c273f2c02ed3f6d27d32da72d9606dd1cac2a2
sirdav1d <ddavid.diniz@gmail.com>
2025-01-17 11:56:40 -0300

    feat: :lipstick: ajuste de cta e alinhamento

--- components/telemdicina/sections/cta.tsx
+++ components/telemdicina/sections/cta.tsx
@@ -8,18 +8,14 @@ import app2 from '@/assets/app2.jpeg'
 
 export default function CTA() {
 	return (
-		<div className='max-w-7xl w-full items-center h-full flex mx-auto gap-20 justify-between z-10 py-10 px-10 m-10  relative'>
+		<div className='max-w-7xl w-full items-center h-full flex mx-auto gap-20 justify-between z-10 py-10 xl:px-10 m-10  relative'>
 			<div className="bg-blue-900 absolute left-0 h-full w-full xl:rounded-3xl bg-[url('/blue-gradient-background-cta.jpg')]" />
 			<div className='z-10 mx-4 xl:mx-10 text-slate-50 flex flex-col gap-5 w-full md:w-1/2'>
 				<h2 className='font-semibold text-3xl xl:text-4xl md:text-balance capitalize md:leading-loose'>
 					Plano Corporativo
 				</h2>
 				<p>
-					Os colaboradores terão acesso a{' '}
-					<strong>teleconsulta 24h por dia 7 dias na semana</strong> e
-					teleconsulta Especializada com agenda Médica com{' '}
-					<strong>diversos especialistas</strong>, tudo isso sem sair do local
-					de trabalho. <br /><br />
+					Os Colaboradores terão acesso a <strong>Teleconsulta 24h por dia, 7 dias por semana</strong> e consultas agendadas com diversos especialistas, tudo isso, <strong>sem sair do local de trabalho.</strong> <br /><br />
 					Nosso plano Corporativo inclui <strong>seguro de vida</strong> que
 					protege a todos aqueles que fazem o{' '}
 					<strong>melhor para sua empresa</strong>. Esses benefícios, trazem
