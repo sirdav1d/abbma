addc3799c7c9dae57ecc226b2e79ef5239d3142d
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2025-01-31 09:03:47 -0300

    feat: improve layout and responsiveness of license component details

--- app/dashboard/license/_components/license.tsx
+++ app/dashboard/license/_components/license.tsx
@@ -31,7 +31,7 @@ export default function License({
 				<Card
 					className={`${isPageShare ? 'max-w-2xl md:scale-125' : 'max-w-lg'}  shadow-lg `}>
 					<CardHeader>
-						<CardTitle className='flex gap-5 items-center'>
+						<CardTitle className='flex flex-col md:flex-row gap-5 md:items-center'>
 							<Image
 								src={logo}
 								alt='logo ABBMA'
@@ -40,15 +40,17 @@ export default function License({
 								className='xl:w-20 w-12 object-contain'></Image>
 							{user?.name}
 						</CardTitle>
-						<CardDescription className='font-semibold mt-5'>
+						<CardDescription className='font-semibold mt-5 text-sm md:text-base'>
 							{user?.email}
 						</CardDescription>
 					</CardHeader>
 					<CardContent className='flex items-center w-full gap-10'>
 						<div className='flex flex-col w-full gap-2 text-sm '>
-							<p>CPF: {user?.cpf}</p>
-							<p>Nascimento: {user?.date_birth}</p>
-							<p className='font-semibold'>
+							<p className='text-sm md:text-base'>CPF: {user?.cpf}</p>
+							<p className='text-sm md:text-base'>
+								Nascimento: {user?.date_birth}
+							</p>
+							<p className='font-semibold text-sm md:text-base'>
 								Tipo de usuário: {isTitular ? 'Titular' : 'Dependente'}
 							</p>
 						</div>
